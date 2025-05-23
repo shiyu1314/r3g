aed00052a8bd2e01985f56f68b48b1739fafbef4
shiyu1314 <shiyu1052@gmail.com>
2025-05-24 07:53:54 +0800

    Update op.sh

--- sh/op.sh
+++ sh/op.sh
@@ -16,7 +16,24 @@ echo 'src-git xd https://github.com/shiyu1314/openwrt-packages' >>feeds.conf.def
 git_sparse_clone openwrt-23.05 https://github.com/immortalwrt/luci modules/luci-base
 git_sparse_clone openwrt-23.05 https://github.com/immortalwrt/luci modules/luci-mod-status
 
-git clone -b openwrt-24.10 --depth 1 --single-branch https://github.com/sbwml/luci-theme-argon package/xd/luci-theme-argon
+git clone -b main --depth 1 --single-branch https://github.com/AngelaCooljx/luci-theme-material3 package/luci-theme-material3
+# luci-theme-material3
+rm -rf package/luci-theme-material3/{.git,Readme.md}
+sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|' package/luci-theme-material3/Makefile
+sed -i '/uci -q delete luci.themes.Material3Red/a \	uci set luci.main.mediaurlbase=\x27/luci-static/bootstrap\x27' package/luci-theme-material3/Makefile
+rm -rf package/luci-theme-material3/root/etc/uci-defaults/30_luci-theme-material3
+echo '#!/bin/sh
+
+if [ "$PKG_UPGRADE" != 1 ]; then
+	uci get luci.themes.material3 >/dev/null 2>&1 || \
+	uci batch <<-EOF
+		set luci.themes.material3=/luci-static/material3
+		set luci.main.mediaurlbase=/luci-static/material3
+		commit luci
+	EOF
+fi
+
+exit 0' > package/luci-theme-material3/root/etc/uci-defaults/30_luci-theme-material3 && chmod +x package/luci-theme-material3/root/etc/uci-defaults/30_luci-theme-material3
 
 
 ./scripts/feeds update -a
